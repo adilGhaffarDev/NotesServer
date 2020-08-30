ff88b66ff73f0049a24ba41b8cd36381986969e9
adil ghaffar <adilghaffar64@gmail.com>
2020-08-31 01:09:51 +0300

    first build created

---
+++ build/precache-manifest.1b7ec387d24dece026e6e9d4b4d0743a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65bf5f5a75259a96488efc4fadc0b60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf8afd7d96d8935c04cd",
+    "url": "/static/js/2.249aa196.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.249aa196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9993418eb06eef31c6c",
+    "url": "/static/js/main.dad6a3b7.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
---
+++ build/precache-manifest.eaa6c8ff7e67b65a0eeb10149171d710.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc7364f5b2d827820237b66f7576135",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf8afd7d96d8935c04cd",
+    "url": "/static/js/2.249aa196.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.249aa196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c702a5b2b071d4ae1d",
+    "url": "/static/js/main.071f9914.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
